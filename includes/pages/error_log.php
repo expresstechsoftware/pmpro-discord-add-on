ee72722a218f5b414a6a9ce25710cc0ed4cb9f9c
Ryan B <ryanbriso@gmail.com>
2024-05-30 22:01:52 +0200

    Update error_log.php
    Edited so that it is read only and will stop at 10k lines otherwise dashboard will not load.

--- includes/pages/error_log.php
+++ includes/pages/error_log.php
@@ -2,14 +2,18 @@
 <?php
 	$uuid     = get_option( 'ets_pmpro_discord_uuid_file_name' );
 	$filename = $uuid . PMPro_Discord_Logs::$log_file_name;
-	$handle   = fopen( WP_CONTENT_DIR . '/' . $filename, 'a+' );
+	$handle   = fopen( WP_CONTENT_DIR . '/' . $filename, 'r' );
+  $line_count = 0;
+  $max_lines = 10000;
   if( $handle ){
-    while ( ! feof( $handle ) ) {
+    while ( ! feof( $handle ) && $line_count < $max_lines ) {
       echo fgets( $handle ) . '<br />';
+      $line_count++;
     }
   }
 	fclose( $handle );
 ?>
+
 </div>
 <div class="pmpro-clrbtndiv">
 	<div class="form-group">
